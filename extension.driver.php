7f0ccf7710507b25c05ef84e2f4f75c134a1d753
Your Hero Paul <paul.garrett@airlock.com>
2009-06-09 10:35:55 +0100

    Updated version number and documentation.

--- extension.driver.php
+++ extension.driver.php
@@ -4,7 +4,7 @@
 
 		public function about(){
 			return array('name' => 'EventEx: multiple section form submission',
-						 'version' => '1.0',
+						 'version' => '1.01',
 						 'release-date' => '2009-01-21',
 						 'author' => array('name' => 'Your Hero Paul',
 										   'website' => 'http://github.com/yourheropaul/',
